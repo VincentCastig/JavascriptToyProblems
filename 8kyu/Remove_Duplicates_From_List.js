bcc7186c9a72e4395e03aabbb92f4c6ef89e4830
VincentCastig <vincent.castig@gmail.com>
2024-03-14 08:17:45 -0500

    delete file

--- 8kyu/Remove_Duplicates_From_List.js
+++
@@ -1,9 +0,0 @@
-// Complete the functionthat removes duplicates from a list of numbers.
-
-// The order of the sequence needs to stay the same.
-
-function distinct(a) {
-    return a.filter( (val,index) => !a.includes(val, index + 1));
-  }
-
-  distinct([1,1,2,3,4,4])
